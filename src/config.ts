3171d93a9736b4dd35974cf1d076fa70d69158ad
Srijan Mukherjee <srijmukh070.gmail.com>
2026-04-16 23:26:51 +0530

    fix: update resume URL in config and remove outdated PDF file

--- src/config.ts
+++ src/config.ts
@@ -9,6 +9,6 @@ export const config = {
   siteTitle: import.meta.env.VITE_SITE_TITLE || "Srijan Mukherjee - Portfolio",
   resumeUrl:
     import.meta.env.VITE_RESUME_URL ||
-    "https://github.com/sinjar666/my-resume/releases/download/v20260405-141321/resume.pdf",
+    "https://github.com/sinjar666/my-resume/releases/download/v20260416-175407/resume.pdf",
   // Add more config as needed
 };
